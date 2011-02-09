9355ea3f262ae020e4483903bd65ad0a7b284aee
Philip (flip) Kromer <flip@infochimps.org>
2011-02-09 15:04:15 +0800

    extended ratings doodad; main page shows more days per request of holly; main page has link to site

--- config/initializers/time_formats.rb
+++
@@ -1,1 +0,0 @@
-Date::DATE_FORMATS[:verbose] = lambda{|time| time.strftime("%A, %b #{time.day.ordinalize}") }
--- db/migrate/009_add_shibboleth_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddShibbolethToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :shibboleth, :string, :null => false, :default => ""
-  end
-
-  def self.down
-    remove_column :users, :shibboleth
-  end
-end
